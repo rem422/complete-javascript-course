0013dee54eccf24587361255b8fcb38a8e4e831b
Rem Simiyu <remsimiyu@gmail.com>
2025-09-08 09:06:32 +0300

    Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,7 @@
     let js = "amazing"; 
     console.log(40 + 8 + 23 -10);
 
+// VALUES AND VARIABLES
     console.log('Jonas');
     console.log(23);
     
@@ -20,4 +21,27 @@
     let myFirstJob = 'Programmer';
     let myCurrentJob = 'Teacher';
 
-    
+//DATA TYPES
+// JavaScript has dynamic typing: We do not have to manually define the data type of the value stored in a variable. Instead, data types are defined automatically.
+
+//premitive data types
+
+//1. Number: Floating point numbers used for decimals and integers
+    let age = 23;
+//2. String: Sequence of characters used for text
+    let middleName = 'Jonas';
+
+//3. Boolean: Logical type that can only be true or false used for taking decisions
+    let fullage = true;
+
+//4. Undefined: Value taken by a variable that is not yet defined('Empty value')
+    let children;
+
+//5. Null: Also means ('Empty value') for now
+    let myAge;
+
+//6. Symbol (ES2015): Value that is unique and cannot be changed [Not useful for now]
+
+//7. Symbol (ES2020): Large integers than the Number type can hold
+
+
