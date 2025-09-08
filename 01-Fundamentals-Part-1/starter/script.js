29987002fc9b9ccd6bca695cc03e0d18675fdbc4
Rem Simiyu <remsimiyu@gmail.com>
2025-09-08 09:20:28 +0300

    Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,25 +1,25 @@
-    let js = "amazing"; 
-    console.log(40 + 8 + 23 -10);
+    // let js = "amazing"; 
+    // console.log(40 + 8 + 23 -10);
 
 // VALUES AND VARIABLES
-    console.log('Jonas');
-    console.log(23);
+//     console.log('Jonas');
+//     console.log(23);
     
-    let firstName = "Jonas";
+//     let firstName = "Jonas";
 
 
-    console.log(firstName);
-    console.log(firstName);
-    console.log(firstName);
+//     console.log(firstName);
+//     console.log(firstName);
+//     console.log(firstName);
     
-    //naming conventions
-    let jonas_matilda ="JM";
-    let $lastName = 'Mathew';
-    let myWork = 'Web Developer';
+//     //naming conventions
+//     let jonas_matilda ="JM";
+//     let $lastName = 'Mathew';
+//     let myWork = 'Web Developer';
 
-//Descriptive naming
-    let myFirstJob = 'Programmer';
-    let myCurrentJob = 'Teacher';
+// //Descriptive naming
+//     let myFirstJob = 'Programmer';
+//     let myCurrentJob = 'Teacher';
 
 //DATA TYPES
 // JavaScript has dynamic typing: We do not have to manually define the data type of the value stored in a variable. Instead, data types are defined automatically.
@@ -28,20 +28,37 @@
 
 //1. Number: Floating point numbers used for decimals and integers
     let age = 23;
+    console.log(typeof age);
+
 //2. String: Sequence of characters used for text
     let middleName = 'Jonas';
+    console.log(typeof middleName);
 
 //3. Boolean: Logical type that can only be true or false used for taking decisions
-    let fullage = true;
+    let javascriptIsFun =  true;
+    console.log(javascriptIsFun);
+
+    console.log(typeof true);
+
+    // dynamic typing in action
+    javascriptIsFun = 'YES!'
+    console.log(typeof javascriptIsFun);
 
 //4. Undefined: Value taken by a variable that is not yet defined('Empty value')
-    let children;
+    let year;
+    console.log(year);
+    console.log(typeof year);
+
+    //reassigning a variable
+    year = 1991;
+    console.log(year);
+    console.log(typeof year);
+    
 
 //5. Null: Also means ('Empty value') for now
-    let myAge;
+console.log(typeof null); // Type of null is object (this is a bug in JavaScript)
 
 //6. Symbol (ES2015): Value that is unique and cannot be changed [Not useful for now]
-
 //7. Symbol (ES2020): Large integers than the Number type can hold
 
 
