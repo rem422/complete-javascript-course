599bb9eec7f0835eeb422da5756993fa4e640264
Rem Simiyu <remsimiyu@gmail.com>
2025-09-08 08:50:56 +0300

    Values and Variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,23 @@
+    let js = "amazing"; 
+    console.log(40 + 8 + 23 -10);
+
+    console.log('Jonas');
+    console.log(23);
+    
+    let firstName = "Jonas";
+
+
+    console.log(firstName);
+    console.log(firstName);
+    console.log(firstName);
+    
+    //naming conventions
+    let jonas_matilda ="JM";
+    let $lastName = 'Mathew';
+    let myWork = 'Web Developer';
+
+//Descriptive naming
+    let myFirstJob = 'Programmer';
+    let myCurrentJob = 'Teacher';
+
+    
