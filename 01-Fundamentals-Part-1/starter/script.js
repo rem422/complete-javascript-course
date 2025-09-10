5cc651e577a3d86758b799dbbaad882da7dd84f6
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2025-09-10 21:03:24 +0300

    Truthy & Falsy values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -247,6 +247,15 @@ n = n - 1;
 console.log(n);
 */
 
+//TRUTHY & FALSY VALUES
+// 5 falsy values:0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+
 
 
 
