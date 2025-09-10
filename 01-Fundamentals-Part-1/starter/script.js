b5e65ca5b67558f9868917422def81437b8e0a20
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2025-09-10 21:37:58 +0300

    Equality Operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -245,7 +245,6 @@ console.log('23' / '2');
 let n = '1' + 1; // '11
 n = n - 1;
 console.log(n);
-*/
 
 //TRUTHY & FALSY VALUES
 // 5 falsy values:0, '', undefined, null, NaN
@@ -269,4 +268,31 @@ if (height) {
 } else {
     console.log(`Height is UNDEFINED`);
 }
+*/
+
+//EQUALITY OPERATORS
+const age = 18;
+if(age === 18) console.log(`You just became an adult :D (strict)`);
+if(age == 18) console.log(`You just became an adult :D (loose)`);
+
+const favourite = Number(prompt(`What's your favourite number`));
+console.log(favourite);
+console.log(typeof favourite);
+
+if(favourite === 23) {// 22 === 23 -> false
+    console.log(`Cool! 23 is an amazing number!`);
+} else if (favourite === 7) {
+    console.log(`7 is also a cool number`);
+} else if (favourite === 9) {
+    console.log(`9 is also a cool number`);
+} else {
+    console.log(`Number is not 23 or 7 or 9`);
+}
+
+if (favourite !== 23) {
+    console.log(`Why not 23?`);
+}
+
+
+
 
