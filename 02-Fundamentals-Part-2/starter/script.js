5ef18653bb1d0008dc735ee8ee967600f49d1de8
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2025-09-11 15:57:48 +0300

    Arrow Function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -32,22 +32,33 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 console.log(Number(23));
-*/
 
 //FUNCTION DECLARATIONS vs. EXPRESSION
-//function declaration can be called before defining
+//function declaration => can be called before defining
 function calcAge1(birthYear) {
     return 2037 - birthYear;
 }
-
 const age1 = calcAge1(1991);
 
 //function expression
 const calcAge2 = function(birthYear) {
     return 2037 - birthYear;
 }
-
 const age2 = calcAge2(1991)
 
 console.log(age1, age2);
+*/
 
+// ARROW FUNCTION
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (firstName, birthYear) => {
+    const age = 2037 - birthYear;
+    const retirementAge = 65 - age;
+    // return retirementAge;
+    return `${firstName} retires in ${retirementAge} years.`;
+}
+
+console.log(yearsUntilRetirement('Jonas', 1991));
