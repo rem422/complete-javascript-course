df45c086bfe0baecf5ea00ed68a47f5b451f091e
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2025-09-11 15:29:53 +0300

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -10,3 +10,25 @@ const interface = 'Audio';
 const private = 54;
 const if = 23;
 */
+
+//FUNCTIONS
+function logger() {
+    console.log(`My name is Jonas`);
+};
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, orange) {
+    const juice = `Juice with ${apples} apples and ${orange} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
