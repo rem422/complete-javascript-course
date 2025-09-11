96d0671a31498a6b633973b512b862584177e129
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2025-09-11 15:16:06 +0300

    Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+"strict mode";
+/* 
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log(`I can Drive :D`);
+
+const interface = 'Audio';
+const private = 54;
+const if = 23;
+*/
