3729a290de487faa4ec8a40fc776e3c717d5f5ca
Rem Simiyu <remsimiyu@gmail.com>
2025-09-12 21:09:38 +0300

    Complete JavaScript Course

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -76,7 +76,6 @@ function fruitProcessor(apples, oranges) {
     const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
     return juice;
 }
-
 console.log(fruitProcessor(2, 4));
 
 
