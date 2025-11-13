8cca42ebe3b839209205ca7ccd266b3bf53e04ee
Rem Simiyu <remsimiyu@gmail.com>
2025-11-13 11:50:26 +0300

    CHALLENGE #!

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -64,19 +64,45 @@ console.log(yearsUntilRetirement('Jonas', 1991));
 console.log(yearsUntilRetirement('Bob', 1980));
 */
 
+// CHALLENGE #1
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));
+
+//Test 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function(avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log(`No team wins...`);
+    }
+}
+checkWinner(scoreDolphins, scoreKoalas);
+
+//Test 2
+scoreDolphins2 = calcAverage(85, 54, 41);
+scoreKoalas2 = calcAverage(23, 34, 27);
+console.log(scoreDolphins2, scoreKoalas2);
+checkWinner(scoreDolphins2, scoreKoalas2);
+
 //FUNCTIONS CALLING OTHER FUNCTIONS
-function cutFruitPieces(fruit) {
-    return fruit * 4;
-}
+// function cutFruitPieces(fruit) {
+//     return fruit * 4;
+// }
 
-function fruitProcessor(apples, oranges) {
-    const applePieces = cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
 
-    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
-    return juice;
-}
-console.log(fruitProcessor(2, 4));
+//     const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+//     return juice;
+// }
+// console.log(fruitProcessor(2, 4));
 
 
 
