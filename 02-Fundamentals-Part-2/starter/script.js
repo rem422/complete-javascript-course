7aac7b4e1552d04bfe61058dc05c1057a544dfc7
Rem Simiyu <remsimiyu@gmail.com>
2025-11-13 21:02:45 +0300

    Iteration The for Loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -251,33 +251,60 @@ console.log(yearsUntilRetirement('Bob', 1980));
 // console.log(jonas.getSummary());
 
 //CHALLENGE #3
-const mark = {
-    fullName: 'Mark Miller',
-    mass: 78,
-    height: 1.69,
-    calcBMI: function() {
-        this.bmi = this.mass / this.height ** 2
-        return this.bmi;
-    }
+// const mark = {
+//     fullName: 'Mark Miller',
+//     mass: 78,
+//     height: 1.69,
+//     calcBMI: function() {
+//         this.bmi = this.mass / this.height ** 2
+//         return this.bmi;
+//     }
+// }
+//
+// const john = {
+//     fullName: 'John Smith',
+//     mass: 92,
+//     height: 1.95,
+//     calcBMI: function() {
+//         this.bmi = this.mass / this.height ** 2
+//         return this.bmi;
+//     }
+// }
+//
+// mark.calcBMI();
+// john.calcBMI();
+//
+// if(mark.bmi > john.bmi) {
+//     console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`);
+// } else if (john.bmi > mark.bmi) {
+//     console.log(`${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${mark.fullName}'s BMI (${mark.calcBMI()})`);
+// } else {
+//     console.log('There is no winner')
+// }
+
+//ITERATION THE FOR LOOP
+// console.log('Lifting wights repetition 1');
+// console.log('Lifting wights repetition 2');
+// console.log('Lifting wights repetition 3');
+// console.log('Lifting wights repetition 4');
+// console.log('Lifting wights repetition 5');
+// console.log('Lifting wights repetition 6');
+// console.log('Lifting wights repetition 7');
+// console.log('Lifting wights repetition 8');
+// console.log('Lifting wights repetition 9');
+// console.log('Lifting wights repetition 10');
+
+//for loops keeps running while the condition is TRUE
+for(let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting wights repetition ${rep}`)
 }
 
-const john = {
-    fullName: 'John Smith',
-    mass: 92,
-    height: 1.95,
-    calcBMI: function() {
-        this.bmi = this.mass / this.height ** 2
-        return this.bmi;
-    }
-}
 
-mark.calcBMI();
-john.calcBMI();
 
-if(mark.bmi > john.bmi) {
-    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`);
-} else if (john.bmi > mark.bmi) {
-    console.log(`${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${mark.fullName}'s BMI (${mark.calcBMI()})`);
-} else {
-    console.log('There is no winner')
-}
+
+
+
+
+
+
+
