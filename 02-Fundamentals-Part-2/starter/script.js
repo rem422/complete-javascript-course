60a9d6b4b2e49539ef82bb80addf3f3433b96d54
Rem Simiyu <remsimiyu@gmail.com>
2025-11-13 23:00:17 +0300

    Looping Arrays, Breaking and Continuing

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -299,11 +299,50 @@ for(let rep = 1; rep <= 10; rep++) {
     console.log(`Lifting wights repetition ${rep}`)
 }
 
+//Looping Arrays, Breaking and Continuing
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Steven', 'Peter'],
+    true
+]
 
+const types = [];
 
+for(let i = 0; i < jonas.length; i++) {
+    //reading jonas array
+    console.log(jonas[i], typeof jonas[i]);
 
+    //filling types array
+    // types[i] = typeof jonas[i]
+    types.push(typeof jonas[i]);
+}
+console.log(types);
 
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
 
+for(let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+//continue and break
+//continue
+console.log('----- ONLY STRINGS ------')
+for(let i = 0; i < jonas.length; i++) {
+    if(typeof  jonas[i] !== 'string') continue;
+    console.log(jonas[i], typeof jonas[i]);
+}
+
+//break
+console.log('----- BREAK WITH NUMBER ------')
+for(let i = 0; i < jonas.length; i++) {
+    if(typeof  jonas[i] === 'number') break;
+    console.log(jonas[i], typeof jonas[i]);
+}
 
 
 
