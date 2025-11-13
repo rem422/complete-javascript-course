9c06e6767d87c0aebf1d9891f487cbabe59c28aa
Rem Simiyu <remsimiyu@gmail.com>
2025-11-13 15:35:04 +0300

    Introduction to objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -170,12 +170,23 @@ console.log(yearsUntilRetirement('Bob', 1980));
 // };
 
 //CHALLENGE #2
-const calcTip = (bill) => {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-}
+// const calcTip = (bill) => {
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// }
+//
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+//
+// console.log(bills, tips, totals);
 
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+//INTRODUCTION TO OBJECTS
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Steven', 'Peter']
+};
 
-console.log(bills, tips, totals);
+console.log(jonas.firstName);
