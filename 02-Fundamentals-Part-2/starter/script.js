05085c0a229a5979ae92768fb8d72f9f8569cd71
Rem Simiyu <remsimiyu@gmail.com>
2025-11-13 23:26:51 +0300

    The while Loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -344,23 +344,46 @@ console.log(yearsUntilRetirement('Bob', 1980));
 //     console.log(jonas[i], typeof jonas[i]);
 // }
 
-//Looping Backwards and Loops in Loops
-const jonas = [
-    'Jonas',
-    'Schmedtmann',
-    2037 - 1991,
-    'teacher',
-    ['Michael', 'Steven', 'Peter'],
-]
+// //Looping Backwards and Loops in Loops
+// const jonas = [
+//     'Jonas',
+//     'Schmedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Michael', 'Steven', 'Peter'],
+// ]
+//
+// for(let i = jonas.length -1; i >= 0; i--) {
+//     console.log(jonas[i]);
+// }
+//
+// for(let exercise = 1; exercise < 4; exercise++) {
+//     console.log(`--------------- Starting exercise ${exercise}`);
+//
+//     for(let rep = 1; rep < 6; rep++) {
+//         console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`)
+//     }
+// }
 
-for(let i = jonas.length -1; i >= 0; i--) {
-    console.log(jonas[i]);
+//The while Loop
+// for(let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting wights repetition ${rep}`)
+// }
+
+let rep = 1;
+while(rep <= 10) {
+    // console.log(`WHILE: Lifting wights repetition ${rep}`)
+    rep++;
 }
 
-for(let exercise = 1; exercise < 4; exercise++) {
-    console.log(`--------------- Starting exercise ${exercise}`);
+let dice = Math.trunc(Math.random() * 6) + 1;
 
-    for(let rep = 1; rep < 6; rep++) {
-        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`)
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if(dice === 6) {
+        console.log('Loop is about to end...');
     }
 }
+
+
