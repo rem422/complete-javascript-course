f8fd4352f1674238283879fdfd7bab48a2724427
Rem Simiyu <remsimiyu@gmail.com>
2025-11-24 20:06:58 +0300

    Using Google, StackOverflow and MDN

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -28,23 +28,23 @@ const tempratures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // - Find min value in temp array
 // - Subtract min from max and return it
 
-// const calcTempAmplitude = (temps) => {
-//     let max = temps[0];
-//     let min = temps[0];
+const calcTempAmplitude = (temps) => {
+    let max = temps[0];
+    let min = temps[0];
 
-//     for(let i = 0; i < temps.length; i++) {
-//         const curTemp = temps[i];
+    for(let i = 0; i < temps.length; i++) {
+        const curTemp = temps[i];
 
-//         if(typeof curTemp !== 'number') continue;
-//         if(curTemp > max) max = curTemp;
-//         if(curTemp < min) min = curTemp;
-//     }
-//     console.log(max, min);
-//     return max - min;
-// };
+        if(typeof curTemp !== 'number') continue;
+        if(curTemp > max) max = curTemp;
+        if(curTemp < min) min = curTemp;
+    }
+    console.log(max, min);
+    return max - min;
+};
 
-// const amplitude = calcTempAmplitude(tempratures);
-// console.log(amplitude);
+const amplitude = calcTempAmplitude(tempratures);
+console.log(amplitude);
 
 // PROBLEM 2:
 // Function should now receive 2 arrays of temps
