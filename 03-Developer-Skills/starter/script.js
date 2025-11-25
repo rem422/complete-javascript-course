dfcb14c58b643030c1ab233992158828c049604a
Rem Simiyu <remsimiyu@gmail.com>
2025-11-25 14:33:17 +0300

    Debugging with the Console and Breakpoints

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -78,51 +78,94 @@ const tempratures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 //DEBUGING
 
-const measureKelvin = () => {
-    const measurement = {
-        type: "temp",
-        unit: 'celcius',
+// const measureKelvin = () => {
+//     const measurement = {
+//         type: "temp",
+//         unit: 'celcius',
 
-        // C) FIND
-        // value: Number(prompt('Degrees celcius')) 
-        value: 10 
-    }
+//         // C) FIND
+//         // value: Number(prompt('Degrees celcius')) 
+//         value: 10 
+//     }
 
-    // B) FIND
-    console.table(measurement);
+//     // B) FIND
+//     console.table(measurement);
     
-    // console.log(measurement.value);
-    // console.warn(measurement.value);
-    // console.error(measurement.value);
+//     // console.log(measurement.value);
+//     // console.warn(measurement.value);
+//     // console.error(measurement.value);
 
-    const kelvin = measurement.value + 273;
-    return kelvin
+//     const kelvin = measurement.value + 273;
+//     return kelvin
+// }
+// // A) IDENTIFY
+// console.log(measureKelvin());
+
+
+// // Using a debugger
+// const calcTempAmplitudeBug = (t1, t2) => {
+//     const temps = t1.concat(t2);
+//     console.log(temps);
+    
+//     let max = 0;
+//     let min = 0;
+
+//     for(let i = 0; i < temps.length; i++) {
+//         const curTemp = temps[i];
+
+//         if(typeof curTemp !== 'number') continue;
+
+//         debugger;
+//         if(curTemp > max) max = curTemp;
+//         if(curTemp < min) min = curTemp;
+//     }
+//     console.log(max, min);
+//     return max - min;
+// };
+
+// const amplitudeBug = calcTempAmplitudeBug([3,5,1], [9,4,5]);
+// // A) IDENTIFY
+// console.log(amplitudeBug);
+
+//CODING CHALLENGE #1
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+/*
+// 1) Understanding the problem
+// - Array transformed to string, separated by ...
+// - What is the X days? Answer: index + 1
+
+// 2) Breaking up into sub-problems
+// - Transform array into string
+// - Transform each element to string with ºC
+// - Strings needs to contain day (index + 1)
+// - Add ... between elements and start and end of string
+// - Log string to console
+*/
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
+
+const printForecast = (arr) => {
+    let str = '';
+
+    for(let i = 0; i < arr.length; i++) {
+        str += `${arr[i]}ºC in ${i + 1} days ... `;
+    }
+    console.log('...' + str);
 }
-// A) IDENTIFY
-console.log(measureKelvin());
+printForecast(data1);
+printForecast(data2);
 
-
-// Using a debugger
-const calcTempAmplitudeBug = (t1, t2) => {
-    const temps = t1.concat(t2);
-    console.log(temps);
-    
-    let max = 0;
-    let min = 0;
-
-    for(let i = 0; i < temps.length; i++) {
-        const curTemp = temps[i];
-
-        if(typeof curTemp !== 'number') continue;
-
-        debugger;
-        if(curTemp > max) max = curTemp;
-        if(curTemp < min) min = curTemp;
-    }
-    console.log(max, min);
-    return max - min;
-};
-
-const amplitudeBug = calcTempAmplitudeBug([3,5,1], [9,4,5]);
-// A) IDENTIFY
-console.log(amplitudeBug);
